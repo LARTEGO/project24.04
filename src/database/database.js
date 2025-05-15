095447c95f7aa1d8087dcaf4c9d251f0ba699dc4
LARTEGO <148080342+LARTEGO@users.noreply.github.com>
2025-05-15 19:07:53 +0400

    upd database
    task and work tabs

--- src/database/database.js
+++ src/database/database.js
@@ -10,6 +10,25 @@ const sqlite3 = require('sqlite3').verbose();
             login TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL
         )
+        CREATE TABLE IF NOT EXISTS work (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            w_type TEXT NOT NULL,
+            owner TEXT
+       )
+        CREATE TABLE IF NOT EXISTS task (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task_name TEXT,
+            work_id INTEGER,
+            task_category INTEGER,
+            task_reason TEXT,
+            dse_code INTEGER,
+            dse_name TEXT,
+            task_status TEXT,
+            product TEXT,
+            result TEXT,
+            termition TEXT,
+            owner TEXT
+       )
        `);
        });
 
