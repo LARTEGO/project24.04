dc52f55522666eddb04ed9175283ecf2c81b2070
Art4 <Luzhbin.ar@gmail.com>
2025-05-15 19:13:28 +0400

    Заготовки к методам по каждой таблице
    По необходимости переделаем в models, скорее всего именно когда ознакомимся со фронтом

---
+++ src/methods/emlpoye.js
@@ -0,0 +1,3 @@
+// методы к данным по таблице employe (список персонала)
+const sqlite3 = require('sqlite3').verbose();
+const db = sqlite3.Database('/db.sqlite'); // подключение к базе данных
---
+++ src/methods/tabel_cheek.js
@@ -0,0 +1,3 @@
+// методы к данным по таблице tabel_cheek (учёт )
+const sqlite3 = require('sqlite3').verbose();
+const db = sqlite3.Database('/db.sqlite'); // подключение к базе данных
