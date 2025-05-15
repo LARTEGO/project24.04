b034161e832299337910ddc1f8292de4cc35a0d0
Art4 <Luzhbin.ar@gmail.com>
2025-05-15 18:56:48 +0400

    добавил комментарии к коду

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 const login = document.getElementById('login-cheek').value;
 const password = document.getElementById('password-cheek').value;
+// получаем введённые данные
 
 document.getElementById('aut').onclick = async () => {
 
@@ -8,11 +9,14 @@ document.getElementById('aut').onclick = async () => {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({password, login})
     });
+    // описание метода
+    // используется метод POST для отправки введённых данных на сравнение с имеющимися в таблице user
 
     const data = await res.json();
 
     if (data.success) {
         window.location = '/main.html';
+        // при совпадении данных перенаправление на главную страницу 
     }
 
     else {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = sqlite3.Database('/db.sqlite');
+const db = sqlite3.Database('/db.sqlite'); // подключение к базе данных
 
 app.post('/login', (req, res) => {
-    const {login, password} = req.body;
+    const {login, password} = req.body; // нужные данные для входа
     db.run('SELECT * FROM user WHERE login = ? AND password = ?', [login, password], (err, row) => {
-
+        // запрос логина и пароля из таблицы user файла db.sqlite
         if (err) return res.json({success:false});
 
         if (row) {
@@ -15,3 +15,4 @@ app.post('/login', (req, res) => {
         }
     });
 });
+// тестовый вариант регистрации
