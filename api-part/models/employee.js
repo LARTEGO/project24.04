80714779a42c6acf061cf504524a2e5c3937c65b
LARTEGO <art.shadrin.266@yandex.ru>
2025-05-27 19:30:33 +0400

    modules upd

--- api-part/models/employee.js
+++ api-part/models/employee.js
@@ -1,17 +1,17 @@
-const db = require('../../api-part/database/db.sqlite');
+const db = require('../database/database.sqlite');
 
-class employee {
+class Employee {
     static create(callback, name, tabNumber, position, department, deptCode, schedule) {
         const add = `INSERT INTO employee (name, tabNumber, position, department, deptCode, schedule) VALUES (?, ?, ?, ?, ?, ?)`;
         db.run(add, [name, tabNumber, position, department, deptCode, schedule], function(err) {
-            callback(err, {id: this.lastId});
+            callback(err, {id: this.lastID});
         });
     }
 
     static get(callback, name, tabNumber, position, department, deptCode, schedule) {
         const take = `SELECT * FROM employee`;
         db.run(take, [name, tabNumber, position, department, deptCode, schedule], function(err) {
-            callback(err, {id: this.lastId});
+            callback(err, {id: this.lastID});
         })
     }
 
