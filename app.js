7178ccdc647c0a07ea3e6c55aa9fa3e1a0de9b58
LARTEGO <art.shadrin.266@yandex.ru>
2025-05-27 22:11:23 +0400

    мини изм
    в app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 7287;
 const db = require('./api-part/database/database.js');
+
+app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
